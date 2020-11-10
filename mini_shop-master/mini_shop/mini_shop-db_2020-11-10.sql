c6e649c6c2db33dcab8aa4c7651c2f609d590e78
SakuraGoWonFish <64237421+SakuraGowonFish@users.noreply.github.com>
2020-11-10 15:39:24 +0800

    Add files via upload

---
+++ mini_shop-master/mini_shop/mini_shop-db_2020-11-10.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1:3306
+-- 產生時間： 2020-11-10 07:37:48
+-- 伺服器版本： 8.0.18
+-- PHP 版本： 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `mini_shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `bill`
+--
+
+CREATE TABLE `bill` (
+  `bill_sn` mediumint(8) UNSIGNED NOT NULL COMMENT '帳單編號',
+  `user_sn` mediumint(8) UNSIGNED NOT NULL COMMENT '購買人',
+  `bill_total` mediumint(8) UNSIGNED NOT NULL COMMENT '總金額',
+  `bill_date` datetime NOT NULL COMMENT '購買日期',
+  `bill_status` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '處理狀態'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `bill_detail`
+--
+
+CREATE TABLE `bill_detail` (
+  `bill_sn` mediumint(8) UNSIGNED NOT NULL COMMENT '帳單編號',
+  `goods_sn` mediumint(8) UNSIGNED NOT NULL COMMENT '商品編號',
+  `goods_amount` tinyint(3) UNSIGNED NOT NULL COMMENT '購買數量',
+  `goods_total` mediumint(8) UNSIGNED NOT NULL COMMENT '小計'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `goods`
+--
+
+CREATE TABLE `goods` (
+  `goods_sn` mediumint(8) UNSIGNED NOT NULL COMMENT '商品編號',
+  `goods_title` varchar(255) NOT NULL COMMENT '商品名稱',
+  `goods_content` text NOT NULL COMMENT '商品說明',
+  `goods_price` mediumint(8) UNSIGNED NOT NULL COMMENT '商品價錢',
+  `goods_counter` smallint(5) UNSIGNED NOT NULL COMMENT '人氣',
+  `goods_date` datetime NOT NULL COMMENT '上架日期'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 傾印資料表的資料 `goods`
+--
+
+INSERT INTO `goods` (`goods_sn`, `goods_title`, `goods_content`, `goods_price`, `goods_counter`, `goods_date`) VALUES
+(1, '鯊魚 50cm', '<p>中等大小鯊魚</p>\r\n\r\n<p>HOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOCHA</p>\r\n', 399, 27, '2020-11-10 06:06:50'),
+(2, '鯊魚 150cm', '<p>超大超軟鯊魚</p>\r\n\r\n<p>AHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOY</p>\r\n', 799, 15, '2020-11-10 06:29:41'),
+(3, '鯊魚吊飾', '', 69, 11, '2020-11-10 06:30:53'),
+(4, '鯊魚抱枕', '<p>跟鯊魚比起來鯊鯊手感沒更柔軟</p>\r\n\r\n<p>但看起來更鯊了</p>\r\n', 4999, 2, '2020-11-10 07:25:21'),
+(5, '鯊鯊吊飾', '<p>可以這很鯊</p>\r\n', 399, 1, '2020-11-10 07:29:32'),
+(6, '鯊鯊吊飾2', '<p>HOOOOOOOOOOOCHA</p>\r\n', 399, 1, '2020-11-10 07:30:31'),
+(7, '鯊鯊滑鼠墊', '<p>滑到你不會想用滑鼠，只想捏那可愛的臉</p>\r\n', 1089, 1, '2020-11-10 07:32:59'),
+(8, '鯊枕', '<p>小鯊枕</p>\r\n\r\n<p>RRRRRRRRRRRRRRRRRRRRR</p>\r\n\r\n<p>AAAAAAAAAAAAAAAAAAAAAAAA</p>\r\n', 499, 1, '2020-11-10 07:34:46');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `users`
+--
+
+CREATE TABLE `users` (
+  `user_sn` mediumint(8) UNSIGNED NOT NULL COMMENT '使用者編號',
+  `user_name` varchar(255) NOT NULL COMMENT '使用者姓名',
+  `user_id` varchar(255) NOT NULL COMMENT '使用者帳號',
+  `user_passwd` varchar(255) NOT NULL COMMENT '使用者密碼',
+  `user_email` varchar(255) NOT NULL COMMENT '使用者信箱',
+  `user_sex` enum('先生','女士') NOT NULL COMMENT '使用者性別',
+  `user_tel` varchar(255) NOT NULL COMMENT '使用者電話',
+  `user_zip` varchar(255) NOT NULL COMMENT '使用者郵遞區號',
+  `user_county` varchar(255) NOT NULL COMMENT '使用者縣市',
+  `user_district` varchar(255) NOT NULL COMMENT '使用者鄉鎮市區',
+  `user_address` varchar(255) NOT NULL COMMENT '使用者地址'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 傾印資料表的資料 `users`
+--
+
+INSERT INTO `users` (`user_sn`, `user_name`, `user_id`, `user_passwd`, `user_email`, `user_sex`, `user_tel`, `user_zip`, `user_county`, `user_district`, `user_address`) VALUES
+(1, '羅盛軒', 'blackstring', '$2y$10$FPqsG3JGIalbmM6Ns8AAT.ePWSFwVWe9WcMrUi.c8wkkvjba1xAKu', 'axsxdc@slfkol.oisjfoi', '先生', '891723', '880', '澎湖縣', '馬公市', 'awe');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`bill_sn`);
+
+--
+-- 資料表索引 `bill_detail`
+--
+ALTER TABLE `bill_detail`
+  ADD PRIMARY KEY (`bill_sn`,`goods_sn`);
+
+--
+-- 資料表索引 `goods`
+--
+ALTER TABLE `goods`
+  ADD PRIMARY KEY (`goods_sn`);
+
+--
+-- 資料表索引 `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_sn`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `bill_sn` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '帳單編號';
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `goods`
+--
+ALTER TABLE `goods`
+  MODIFY `goods_sn` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '商品編號', AUTO_INCREMENT=9;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_sn` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '使用者編號', AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
